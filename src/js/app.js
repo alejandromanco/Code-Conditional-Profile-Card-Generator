34158ba25c682b11447aa3cb9113b9c2a36d427f
alejandromanco <alejandro.manco@hotmail.com>
2022-08-03 10:04:32 +0000

    cambios

--- src/js/app.js
+++ src/js/app.js
@@ -31,19 +31,35 @@ function render(variables = {}) {
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+  ${cover}
+<img src="${variables.avatarURL}" class="photo" />
+<h1>${variables.name ? variables.name : "Name"} ${
+    variables.lastname ? variables.lastname : "Last name"
+  }</h1>
+<h2>${variables.role ? variables.role : "Role"}</h2>
+<h3>${variables.city ? variables.city : "City"}, ${
+    variables.country ? variables.country : "Country"
+  }</h3>
+<ul class="${
+    variables.socialMediaPosition
+      ? variables.socialMediaPosition
+      : "position-right"
+  }">
+  <li><a href="${
+    variables.twitter
+      ? "https://twitter.com/" + variables.twitter
+      : "https://twitter.com/"
+  }"><i class="fab fa-twitter"></i></a></li>
+  <li><a href="${
+    variables.github === "alesanchezr"
+      ? "https://github.com/alejandromanco"
+      : "https://github.com/4GeeksAcademy"
+  } "><i class="fab fa-github"></i></a></li>
+  <li><a href="https://www.linkedin.com/in/alejandro-manco-alvarez-76057215b/"><i class="fab fa-linkedin"></i></a></li>
+  <li><a href="https://www.instagram.com/alejandromanco18/"><i class="fab fa-instagram"></i></a></li>
+</ul>
+</div>
+`;
 }
 
 /**
@@ -54,9 +70,11 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://render.fineartamerica.com/images/rendered/search/print/24/24/break/images-medium-5/once-upon-a-time-phil-koch.jpg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQE9F4JRoeYJ-A/profile-displayphoto-shrink_800_800/0/1525833786082?e=1665014400&v=beta&t=vtzE6-erJQvS5lltelJe0-way82fyx_qMD6z0P1uOvc",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
